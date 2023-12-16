13a1fca404a2e12e011540c17ce66c9c13248759
ram pravesh singh <rampraveshsingh1996@gmail.com>
2023-12-17 01:15:09 +0530

    created multi language support in gpt search page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,44 +4,74 @@ import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
-import { LOGO } from "../utils/constants";
+import { LOGO, SUPPORTED_LANGUAGES } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
-  const dispatch = useDispatch()
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+const dispatch = useDispatch();
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
         // Sign-out successful.
-      })
-      .catch((error) => {
-      });
+      })  
+      .catch((error) => {});
   };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        const { uid, email, displayName,photoURL } = user;
-        dispatch(addUser({ uid: uid, email: email, displayName: displayName,photoURL:photoURL }));
-        navigate("/browse")
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
       } else {
-        dispatch(removeUser())
-        navigate("/")
+        dispatch(removeUser());
+        navigate("/");
       }
     });
-    return () => unsubscribe()
+    return () => unsubscribe();
   }, []);
+
+  const handleGptSearchClick = () => {
+    dispatch(toggleGptSearchView());
+  };
+  const handleLanguagechange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img
-        className="w-44"
-        src={LOGO}
-        alt=""
-      />
+      <img className="w-44" src={LOGO} alt="" />
       {user && (
         <div className="flex p-2">
-    <img className="w-12 h-12 " src={user?.photoURL} alt="userIcon" />
+          {showGptSearch && (
+            <select
+              className="p-2 bg-gray-900 text-white m-2"
+              onChange={handleLanguagechange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="py-4 px-4 m-2 bg-purple-800 text-white rounded-lg"
+            onClick={handleGptSearchClick}
+          >
+           {!showGptSearch ? 'GPT Search' : 'Home Page'}
+          </button>
+          <img className="w-12 h-12 " src={user?.photoURL} alt="userIcon" />
           <button onClick={handleSignOut} className="font-bold text-white">
             (Sign Out)
           </button>
