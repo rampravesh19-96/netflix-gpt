c46ddd0b5b26ae57819fdd88943616e0b7cac1bb
rampravesh19-96 <64700001+rampravesh19-96@users.noreply.github.com>
2023-12-30 21:36:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Provider } from "react-redux";
 import Body from "./components/Body";
 import appStore from "./utils/appStore";
-
+// App.js file
 function App() {
   return (
     <Provider store={appStore}>
